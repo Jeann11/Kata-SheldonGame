fdbbeaf84f8cabfbd87651cd5904c8702ff5ccf5
Jeann11 <jeanneth8464@gmail.com>
2022-12-27 19:06:50 +0100

    test paper covers rocks & reverse

--- __test__/SheldonGame.test.js
+++ __test__/SheldonGame.test.js
@@ -2,16 +2,20 @@ const play = require("../src/SheldonGame");
 
 describe('Sheldon Game', () => {
 
-    test('Player 1 Win: scissors cut paper', () => {
+    test('Player 1 Win: scissors cuts paper', () => {
         expect(play("scissors", "paper")).toBe("Player 1 Win");
     });
 
-    test('Player 2 Win: paper loose scissors', () => {
+    test('Player 2 Win: scissors cuts paper', () => {
         expect(play("paper", "scissors")).toBe("Player 2 Win");
     });
 
-    test('Player 1 Win: paper beats rock', () => {
+    test('Player 1 Win: paper covers rock', () => {
         expect(play("paper", "rock")).toBe("Player 1 Win");
+    });
+
+    test('Player 2 Win: paper covers rock', () => {
+        expect(play("rock", "paper")).toBe("Player 2 Win");
     });
 
 
--- src/SheldonGame.js
+++ src/SheldonGame.js
@@ -4,12 +4,14 @@ function play(handPlayer1, handPlayer2) {
     let result
 
     if (handPlayer1 == "scissors" && handPlayer2 == "paper" ||
-        handPlayer1 == "paper" && handPlayer2 == "rock"
+        handPlayer1 == "paper" && handPlayer2 == "rock" 
     ) {
         result = "Player 1 Win"
     }
 
-    if (handPlayer2 == "scissors" && handPlayer1 == "paper") {
+    if (handPlayer2 == "scissors" && handPlayer1 == "paper" ||
+        handPlayer2 == "paper" && handPlayer1 == "rock"
+    ) {
         result = "Player 2 Win"
     }
 
